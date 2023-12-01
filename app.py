b79a388bb4b135dac45398e2cd0cec5fa3ccf88c
Hojiakbar <112841762+Kopperlive@users.noreply.github.com>
2023-12-01 10:42:25 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -12,16 +12,46 @@ def get_db_connection():
 def index():
     return render_template('admin_panel.html')
 
-@app.route('/add_courier', methods=['GET', 'POST'])
-def add_courier():
+# --- Dish Management ---
+@app.route('/add_dish', methods=['GET', 'POST'])
+def add_dish():
     if request.method == 'POST':
         name = request.form['name']
+        price = request.form['price']
+        category_id = request.form['category_id']
         conn = get_db_connection()
-        conn.execute('INSERT INTO couriers (name) VALUES (?)', (name,))
+        conn.execute('INSERT INTO dishes (name, price, category_id, sub_category_id) VALUES (?, ?, ?, ?)', (name, price, category_id, sub_category_id))
+        categories = conn.execute('SELECT * FROM categories').fetchall()
+        sub_category = conn.execute('SELECT * FROM sub_category').fetchall()
+        conn.commit()
+        conn.close()
+        return render_template('add_dish.html', categories=categories, sub_category=sub_category)
+    conn = get_db_connection()
+    categories = conn.execute('SELECT * FROM categories').fetchall()
+    conn.close()
+    return render_template('add_dish.html', categories=categories)
+    
+@app.route('/edit_dish/<int:dish_id>', methods=['GET', 'POST'])
+def edit_dish(dish_id):
+    if request.method == 'POST':
+        # Logic to edit a dish
+        conn = get_db_connection()
+        conn.execute('UPDATE dishes SET name = ?, price = ?, category_id = ? WHERE id = ?', (name, price, category_id, dish_id))
         conn.commit()
         conn.close()
         return redirect(url_for('admin_panel'))
-    return render_template('add_courier.html')
+    return render_template('edit_dish.html', dish_id=dish_id)
+
+@app.route('/delete_dish/<int:dish_id>', methods=['POST'])
+def delete_dish(dish_id):
+    # Logic to delete a dish
+    conn = get_db_connection()
+    conn.execute('DELETE FROM dishes WHERE id = ?', (dish_id,))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('admin_panel'))
+
+# --- Category Management ---
 
 @app.route('/add_category', methods=['GET', 'POST'])
 def add_category():
@@ -34,49 +64,162 @@ def add_category():
         return redirect(url_for('admin_panel'))
     return render_template('add_category.html')
 
-@app.route('/add_dish', methods=['GET', 'POST'])
-def add_dish():
+@app.route('/edit_category/<int:category_id>', methods=['GET', 'POST'])
+def edit_category(category_id):
     if request.method == 'POST':
+        # Logic to edit a category
         name = request.form['name']
-        price = request.form['price']
         category_id = request.form['category_id']
         conn = get_db_connection()
-        conn.execute('INSERT INTO dishes (name, price, category_id) VALUES (?, ?, ?)', (name, price, category_id))
-        categories = conn.execute('SELECT * FROM categories').fetchall()
+        conn.execute('UPDATE categories SET name = ? WHERE id = ?', (name, category_id))
         conn.commit()
         conn.close()
-        return render_template('add_dish.html', categories=categories)
+        return redirect(url_for('admin_panel'))
+    # Fetch category details to populate the form
     conn = get_db_connection()
-    categories = conn.execute('SELECT * FROM categories').fetchall()
+    cur.execute('''Select * from categories where id = ?''', (category_id,))
+    category = cur.fetchall()
     conn.close()
-    return render_template('add_dish.html', categories=categories)
+    return render_template('edit_category.html', category=category)
 
+@app.route('/delete_category/<int:category_id>', methods=['POST'])
+def delete_category(category_id):
+    # Logic to delete a category
+    conn = get_db_connection()
+    conn.execute('DELETE FROM categories WHERE id = ?', (category_id,))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('admin_panel'))
+
+# --- Courier Management ---
+@app.route('/add_courier', methods=['GET', 'POST'])
+def add_courier():
+    if request.method == 'POST':
+        name = request.form['name']
+        conn = get_db_connection()
+        conn.execute('INSERT INTO couriers (name) VALUES (?)', (name,))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('admin_panel'))
+    return render_template('add_courier.html')
+
+@app.route('/edit_courier/<int:courier_id>', methods=['GET', 'POST'])
+def edit_courier(courier_id):
+    # Edit courier logic
+    if request.method == 'POST':
+        # Logic to edit a courier
+        conn = get_db_connection()
+        conn.execute('UPDATE couriers SET name = ? WHERE id = ?', (name, courier_id))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('admin_panel'))
+    return render_template('edit_courier.html', courier_id=courier_id)
+
+@app.route('/delete_courier/<int:courier_id>', methods=['POST'])
+def delete_courier(courier_id):
+    # Delete courier logic
+    conn = get_db_connection()
+    conn.execute('DELETE FROM couriers WHERE id = ?', (courier_id,))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('admin_panel'))
+
+@app.route('/couriers')
+def couriers():
+    # Display a list of couriers
+    conn = get_db_connection()
+    cur.execute('''Select * from users where role = 'courier''')
+    couriers = cur.fetchall()
+    conn.close()
+    return render_template('couriers.html', couriers=couriers)
+
+# --- Order Management ---
+@app.route('/orders')
+def orders():
+    # Display orders
+    conn = get_db_connection()
+    cur.execute('''Select * from orders''')
+    orders = cur.fetchall()
+    conn.close()
+    return render_template('orders.html', orders=orders)
+
+@app.route('/send_order_to_kitchen/<int:order_id>', methods=['POST'])
+def send_order_to_kitchen(order_id):
+    # Send order to kitchen logic
+    conn = get_db_connection()
+    conn.execute('UPDATE orders SET status = ? WHERE id = ?', ('In Kitchen', order_id))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('orders'))
+
+@app.route('/order_details/<int:order_id>')
+def order_details(order_id):
+    # View detailed information about the order
+    conn = get_db_connection()
+    cur.execute('''Select * from orders where id = ?''', (order_id,))
+    order = cur.fetchall()
+    conn.close()
+    return render_template('order_details.html', order_id=order_id)
+
+# --- Branch Management ---
+
+@app.route('/add_branch', methods=['GET', 'POST'])
+def add_branch():
+    if request.method == 'POST':
+        # Logic to add a branch
+        return redirect(url_for('admin_panel'))
+    return render_template('add_branch.html')
+
+@app.route('/edit_branch/<int:branch_id>', methods=['GET', 'POST'])
+def edit_branch(branch_id):
+    if request.method == 'POST':
+        # Logic to edit a branch
+        return redirect(url_for('admin_panel'))
+    # Fetch branch details to populate the form
+    return render_template('edit_branch.html', branch_id=branch_id)
+
+@app.route('/delete_branch/<int:branch_id>', methods=['POST'])
+def delete_branch(branch_id):
+    # Logic to delete a branch
+    conn = get_db_connection()
+    conn.execute('DELETE FROM branch WHERE id = ?', (branch_id,))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('admin_panel'))
+
+@app.route('/branches')
+def branches():
+    # Logic to display a list of branches
+    conn = get_db_connection()
+    cur.execute('''Select * from branch''')
+    branches = cur.fetchall()
+    conn.close()
+    return render_template('branches.html', branches=branches)
+
+# --- Promo Code Management ---
 @app.route('/add_promocode', methods=['GET', 'POST'])
 def add_promocode():
     if request.method == 'POST':
         code = request.form['code']
         discount = request.form['discount']
         conn = get_db_connection()
-        conn.execute('INSERT INTO promocodes (code, discount) VALUES (?, ?)', (code, discount))
+        conn.execute('INSERT INTO promocodes (code, discount) VALUS (?, ?)', (code, discount))
         conn.commit()
         conn.close()
         return redirect(url_for('admin_panel'))
     return render_template('add_promocode.html')
 
-@app.route('/menu')
-def menu():
+@app.route('/delete_promocode/<int:promo_id>', methods=['POST'])
+def delete_promocode(promo_id):
+    # Delete promo code logic
     conn = get_db_connection()
-    categories = conn.execute('SELECT * FROM categories').fetchall()
-    menu = {}
-    for category in categories:
-        dishes = conn.execute('SELECT * FROM dishes WHERE category_id = ?', (category['id'],)).fetchall()
-        menu[category['name']] = dishes
+    conn.execute('DELETE FROM promocodes WHERE id = ?', (promo_id,))
+    conn.commit()
     conn.close()
-    return render_template('menu.html', menu=menu)
+    return redirect(url_for('admin_panel'))
 
-@app.route('/admin')
-def admin_panel():
-    return render_template('admin_panel.html')
+# --- Additional Features ---
+# Implement routes and logic for any additional features required
 
 if __name__ == '__main__':
     app.run(debug=True)
--- database_create.py
+++ database_create.py
@@ -4,9 +4,11 @@ def create_database():
     conn = sqlite3.connect('cooksoo_cafe.db')
     cur = conn.cursor()
 
-    cur.execute('''CREATE TABLE IF NOT EXISTS couriers (
+    cur.execute('''CREATE TABLE IF NOT EXISTS user (
                    id INTEGER PRIMARY KEY AUTOINCREMENT,
                    name TEXT NOT NULL
+                   phone TEXT NOT NULL,
+                   role TEXT NOT NULL
                    )''')
 
     cur.execute('''CREATE TABLE IF NOT EXISTS categories (
@@ -19,6 +21,7 @@ def create_database():
                    name TEXT NOT NULL,
                    price REAL NOT NULL,
                    category_id INTEGER,
+                   img_link TEXT,
                    FOREIGN KEY (category_id) REFERENCES categories (id)
                    )''')
 
@@ -27,6 +30,27 @@ def create_database():
                    code TEXT NOT NULL,
                    discount REAL NOT NULL
                    )''')
+    
+    cur.execute('''CREATE TABLE IF NOT EXISTS orders (
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   description TEXT,
+                   status TEXT,
+                   Foreign Key (courier_id) REFERENCES couriers (id),
+                     Foreign Key (dish_id) REFERENCES dishes (id),
+                   )''')
+
+    cur.execute('''CREATE TABLE IF NOT EXISTS branch ()
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT NOT NULL,
+                    address TEXT NOT NULL,
+                    phone TEXT NOT NULL,
+                     ''')
+
+    cur.execute('''CREATE TABLE IF NOT EXISTS sub_category (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT NOT NULL,
+                    FOREIGN KEY (category_id) REFERENCES categories (id)
+                    )''')
 
     conn.commit()
     conn.close()
